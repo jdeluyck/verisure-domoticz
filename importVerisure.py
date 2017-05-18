bbd188564d44f9a3b7ff2607fd0dc482f8f78704
jdeluyck <sub+github@kcore.org>
2017-05-18 21:41:07 +0200

    Split common functions to verisure_domoticz.py

--- importVerisure.py
+++ importVerisure.py
@@ -1,80 +1,24 @@
 #!/usr/bin/python3
 
-import argparse
 import arrow
-import configparser
 import logging
 import json
-import os.path
 import urllib.request 
 import verisure
 
-# Version 0.2
+from verisure_domoticz import parseArgs, parseConfig
+
+# Version 0.3
 # Author: Jan De Luyck (jan@kcore.org) 
+# Licensed under the GPL v3.0
 #
 # This script depends on the verisure module by Per Sandström, 
 # which you can find at https://github.com/persandstrom/python-verisure
 #
-# Notes: if you want additional sensors to be added, supply me with the 
-# output of 
+# Notes: if you want additional sensors to be added, supply me with the output of 
 #	vsure 'user' 'password' overview 
 
 #################################
-def parseArgs():
-	parser = argparse.ArgumentParser(description = 'Import Verisure information into Domoticz', prog='importVerisure.py')
-	parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.2')
-	parser.add_argument('-l', '--log', dest='logLevel',type=str, choices=['info', 'warning', 'error', 'debug'], help='Specifies the loglevel to be used')
-	parser.add_argument('-c', '--config', dest='configFile', default='vsure.ini', type=str, help='Name of the configuration file to use (default: %(default)s')
-
-	results = parser.parse_args()
-							
-	return vars(results)
-
-def parseConfig(configFile):
-	config = configparser.ConfigParser()
-	
-	if not os.path.isfile(configFile):
-		logging.debug('Config file %s does not exist, creating dummy', configFile)
-		# create a 'new' config file and write it
-		config['domoticz'] = { 'protocol' : 'http', 'host':'localhost', 'port':'8080'}
-		config['verisure'] = { 'username':'', 'password':''}
-		config['global'] = { 'loglevel':'warning', 'timezone':'local'}
-		config['sensorindex'] = { 'sms count':'XX', 'arm state':'XX', 'AAAA BBBB':'XX'}
-		
-		try:
-			with open(configFile, 'w') as file:
-				config.write(file)
-		except IOError as e:
-			logging.error ('Error when writing the default (empty) config file %s: %s', configFile, str(e.reason))
-		else:
-			logging.warning ('A default (empty) config file was written as %s. Please review and re-run this script.', configFile)
-
-		exit (1)
-	else:
-		config.read(configFile)
-
-		# Verify the basics of the config
-		requiredKeys = {}
-		requiredKeys['domoticz'] = {'protocol', 'host', 'port'}
-		requiredKeys['verisure'] = {'username', 'password'}
-		requiredKeys['global'] = {'loglevel', 'timezone'}
-		requiredKeys['sensorindex'] = {'sms count', 'arm state'}
-
-		for section in requiredKeys:
-			if not section in config:
-				logging.error ('Error: section %s is missing. Please check your config file %s!', section, configFile)
-				exit(1)
-			else:
-				for key in requiredKeys[section]:
-					if not key in config[section]:
-						logging.error ('Error: mandatory key %s is missing in section %s. Please check your config file %s!', key, section, configFile)
-						exit(1)
-					elif not config[section][key]:
-						logging.error ('Error: mandatory key %s is empty in section %s. Please check your config file %s!', key, section, configFile)
-						exit(1)
-		
-		# If we survive here, I guess we have a valid config.
-		return config
 		
 def callDomoticz(url):
 	logging.debug ('** Entered CallDomoticz(%s) **', url)
@@ -245,7 +189,7 @@ def processUpdates(deviceType, sensorIdx, deviceLastUpdated, device):
 def main():
 	global domoticzUrl, config
 	# Parse command line
-	arguments = parseArgs()
+	arguments = parseArgs('importVerisure.py')
 
 	# Read config
 	config = parseConfig(arguments['configFile'])
